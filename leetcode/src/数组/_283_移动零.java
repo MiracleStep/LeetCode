ec019c81114cd4f5b566876ca2338e16f3be4e46
suxiang <2585546823@qq.com>
2024-01-08 22:02:30 +0800

    数组

---
+++ leetcode/src/数组/_283_移动零.java
@@ -0,0 +1,15 @@
+package 数组;
+
+public class _283_移动零 {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                nums[slow++] = nums[fast];
+            }
+        }
+        for(;slow < nums.length; slow++) {
+            nums[slow] = 0;
+        }
+    }
+}
--- leetcode/src/数组/_59_螺旋矩阵II.java
+++ leetcode/src/数组/_59_螺旋矩阵II.java
@@ -5,23 +5,25 @@ public class _59_螺旋矩阵II {
         new _59_螺旋矩阵II().generateMatrix(3);
     }
     public int[][] generateMatrix(int n) {
-        int[][] ans = new int[n][n];
-        int[][] dir = {{0, 1},{1, 0},{0, -1},{-1, 0}};
+        int[][] res = new int[n][n];
+        int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int x = 0, y = 0;
         int dirIndex = 0;
-        int x = 0;
-        int y = -1;
-        for (int i = 1; i <= n*n; i++){
+
+        for (int i = 1; i <= n * n; i++) {
+            res[x][y] = i;
+
             x += dir[dirIndex][0];
             y += dir[dirIndex][1];
-            if ((x >= n || y >= n || x < 0 || y < 0) || ans[x][y] != 0){
+            if ((x >= n || y >= n || x < 0 || y < 0) || res[x][y] != 0) {
+                //越界处理
                 x -= dir[dirIndex][0];
                 y -= dir[dirIndex][1];
                 dirIndex = (dirIndex + 1) % 4;
-                i--;
-                continue;
+                x += dir[dirIndex][0];
+                y += dir[dirIndex][1];
             }
-            ans[x][y] = i;
         }
-        return ans;
+        return res;
     }
 }
