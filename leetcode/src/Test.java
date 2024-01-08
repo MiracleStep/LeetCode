15fca4de36dbdbba33db6b981cd4538837c51bcd
suxiang <2585546823@qq.com>
2024-01-08 22:03:05 +0800

    新增通过解题类和测试类

--- leetcode/src/Test.java
+++ leetcode/src/Test.java
@@ -5,10 +5,5 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) throws ParseException {
-        Deque<Integer> deque = new LinkedList<>();
-        deque.addLast(123);
-        deque.addLast(345);
-        deque.offer(333);
-        System.out.println(deque);
     }
 }
