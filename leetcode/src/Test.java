4b3c2a7e48ac3b144036c9e2b72b98b6550e5b99
suxiang <2585546823@qq.com>
2024-01-12 22:45:39 +0800

    前缀和+哈希表

--- leetcode/src/Test.java
+++ leetcode/src/Test.java
@@ -5,5 +5,13 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) throws ParseException {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addLast(123);
+        deque.addLast(345);
+        deque.offer(333);// == add() 函数
+        deque.addFirst(67);
+        System.out.println(deque);//[67, 123, 345, 333]
+        System.out.println(deque.peek());//67
+        System.out.println(deque.remove());//67
     }
 }
