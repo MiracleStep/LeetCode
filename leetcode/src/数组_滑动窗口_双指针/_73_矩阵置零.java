84e05c1b84aa94214919276f18187813a1667bce
suxiang <2585546823@qq.com>
2024-01-21 15:31:49 +0800

    LeetCode HOT100: 矩阵

---
+++ leetcode/src/数组_滑动窗口_双指针/_73_矩阵置零.java
@@ -0,0 +1,21 @@
+package 数组_滑动窗口_双指针;
+
+import java.util.Arrays;
+
+public class _73_矩阵置零 {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    Arrays.fill(matrix[i], 0);
+                    Arrays.fill(matrix, 0);
+                }
+            }
+        }
+    }
+}
