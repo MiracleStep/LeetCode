5ec244036fafdee1e9315692cd69dad8ec07c390
suxiang <2585546823@qq.com>
2024-01-30 19:34:04 +0800

    LeetCode HOT100: 链表

---
+++ leetcode/src/链表/_234_回文链表.java
@@ -0,0 +1,31 @@
+package 链表;
+
+public class _234_回文链表 {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head, fast = head;
+        ListNode pre = null, prepre = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+            pre.next = prepre; //调整前面节点next的方向
+            prepre = pre;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (pre != null && slow != null) {
+            if (pre.val != slow.val) {
+                return false;
+            }
+            pre = pre.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
