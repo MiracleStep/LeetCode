2fefad9ad46d21dcbf07dda339e5fe174e07d972
suxiang <2585546823@qq.com>
2024-02-18 13:01:49 +0800

    LeetCode HOT100: 回溯

--- leetcode/src/回溯法/_78_子集.java
+++ leetcode/src/回溯法/_78_子集.java
@@ -2,24 +2,29 @@ package 回溯法;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class _78_子集 {
+    List<List<Integer>> res = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if(nums.length == 0){
-            return res;
-        }
-        List<Integer> path = new ArrayList<>();
-        dfs(nums, 0, path, res);
+        if (nums.length == 0) return res;
+        Stack<Integer> stack = new Stack<>();
+        dfs(nums, stack, 0);
         return res;
     }
 
-    public void dfs(int[] nums, int index, List<Integer> path, List<List<Integer>> res){
-        res.add(new ArrayList<>(path));
-        for(int i = index; i < nums.length; i++){
-            path.add(nums[i]);
-            dfs(nums, i + 1, path, res);
-            path.remove(path.size() - 1);
+    public void dfs(int[] nums, Stack<Integer> stack, int index) {
+        if (index == nums.length) {
+            res.add(new ArrayList<>(stack));
+            return;
         }
+        //不取
+        dfs(nums, stack, index + 1);
+        //取
+        stack.push(nums[index]);
+        dfs(nums, stack, index + 1);
+        //回溯
+        stack.pop();
     }
 }
