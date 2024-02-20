8de9defdd538b37bb32298f0784d552038349ee9
suxiang <2585546823@qq.com>
2024-02-20 20:21:52 +0800

    LeetCode HOT100: 回溯

--- leetcode/src/回溯法/_22_括号生成.java
+++ leetcode/src/回溯法/_22_括号生成.java
@@ -4,26 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _22_括号生成 {
+    List<String> res = new ArrayList<>();
+
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-        if(n == 0){
+        if (n == 0) {
             return res;
         }
-        dfs("", n, n, res);
+        dfs(new StringBuilder(), n, n);
         return res;
     }
 
-    public void dfs(String cur, int left, int right, List<String> res){
-        if(left == 0 && right == 0){
-            res.add(cur);
+
+    private void dfs(StringBuilder sb, int left, int right) {
+        if (left == 0 && right == 0) {
+            res.add(sb.toString());
             return;
         }
-        if(left > 0){
-            dfs(cur + "(", left - 1, right, res);
-        }
 
-        if(left < right){
-            dfs(cur + ")", left, right - 1, res);
+        if(left > 0) {
+            sb.append("(");
+            dfs(sb, left - 1, right);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if (right > 0 && right > left) {
+            sb.append(")");
+            dfs(sb, left, right - 1);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
--- leetcode/src/回溯法/_79_单词搜索.java
+++ leetcode/src/回溯法/_79_单词搜索.java
@@ -1,19 +1,15 @@
 package 回溯法;
 
 public class _79_单词搜索 {
-    public static void main(String[] args) {
-        boolean abcced = new _79_单词搜索().exist(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED");
-        System.out.println(abcced);
-    }
-    int m, n;
-    int[][] dir = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
     public boolean exist(char[][] board, String word) {
-        m = board.length;
-        n = board[0].length;
-        boolean[][] visited = new boolean[m][n];
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(dfs(board, word, i, j, 0, visited)){
+        if (word.length() == 0) {
+            return true;
+        }
+        boolean[][] vis = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, word, i, j, vis, 0)) {
                     return true;
                 }
             }
@@ -21,47 +17,27 @@ public class _79_单词搜索 {
         return false;
     }
 
-    public boolean dfs(char[][] board, String word, int x, int y, int index, boolean[][] visited){
-        if(visited[x][y] || board[x][y] != word.charAt(index)){
+    private boolean dfs(char[][] board, String word, int x, int y, boolean[][] vis, int index) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length
+                || vis[x][y] || word.charAt(index) != board[x][y]) {
+            //越界、遍历过、字符不匹配
             return false;
         }
-        if(index == word.length() - 1){
+        if (index == word.length() - 1) {
+            //到达最后
             return true;
         }
-        visited[x][y] = true;
-        for(int i = 0; i < 4; i++){
+
+        vis[x][y] = true;
+        for (int i = 0; i < 4; i++) {
             int nx = x + dir[i][0];
             int ny = y + dir[i][1];
-            if(nx >= m || nx < 0 || ny >= n || ny < 0){
-                //越界检查
-                continue;
-            }
-            if(dfs(board, word, nx, ny, index + 1, visited)) return true;
-        }
-        visited[x][y] = false;
-        return false;
-    }
-
-    //时间&空间复杂度优化
-    public boolean exist2(char[][] board, String word) {
-        for(int i = 0;i < board.length;i++){
-            for(int j = 0;j < board[0].length;j++){
-                //遍历每个点以确保每个点都能看看是否满足
-                if(dfs2(board,word,0,i,j)) return true;
+            //直接进行四方向遍历
+            if (dfs(board, word, nx, ny, vis, index + 1)) {
+                return true;
             }
         }
-        //board长度为0也可以不用单独判断
+        vis[x][y] = false;
         return false;
-    }
-    public boolean dfs2(char[][] b,String w,int l,int x,int y){
-        //这里是当二维数组超出范围或者b[x][y]并不是我们需要的对应的那个字母
-        if(x < 0 || x > b.length - 1 || y < 0 || y > b[0].length - 1 || b[x][y] != w.charAt(l)) return false;
-        //字母的正确性在上面已经验证，只要长度一样即为成功
-        if(l == w.length() - 1) return true;
-        b[x][y] = '.'; // 已经遍历过了
-        //然后dfs其他四个方向，长度加1
-        boolean ans = dfs2(b,w,l + 1,x - 1,y) || dfs2(b,w,l + 1,x + 1,y) || dfs2(b,w,l + 1,x,y - 1) || dfs2(b,w,l + 1,x,y + 1);
-        b[x][y] = w.charAt(l);
-        return ans;
     }
 }
