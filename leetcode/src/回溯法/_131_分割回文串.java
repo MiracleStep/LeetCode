a9d2ed24094d1e52d0ebf95d647203f70d16cb56
suxiang <2585546823@qq.com>
2024-02-22 18:43:45 +0800

    LeetCode HOT100: 回溯

---
+++ leetcode/src/回溯法/_131_分割回文串.java
@@ -0,0 +1,57 @@
+package 回溯法;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class _131_分割回文串 {
+    List<List<String>> res = new ArrayList<>();
+
+    public List<List<String>> partition(String s) {
+        int len = s.length();
+        if (len == 0) {
+            return res;
+        }
+
+        Deque<String> deque = new LinkedList<>();
+
+        //动态规划：回文子串题目
+        boolean[][] dp = new boolean[len][len];//s[i]-s[j]是否为回文串
+        for (int i = 0; i < len; i++) {//初始化
+            dp[i][i] = true;
+        }
+        //确定状态转移方程和状态转移顺序
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i + 1; j < len; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i == 1) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
+        //开始dfs
+        dfs(s, deque, 0, dp);
+        return res;
+    }
+
+    private void dfs(String s, Deque<String> deque, int index, boolean[][] dp) {
+        //递归基
+        if (index == s.length()) {
+            res.add(new ArrayList<>(deque));
+            return;
+        }
+        //判断条件 无
+        //遍历
+        for (int i = index; i < s.length(); i++) {
+            if (dp[index][i]) {
+                deque.offer(s.substring(index, i + 1));
+                dfs(s, deque, i + 1, dp);
+                deque.removeLast();
+            }
+        }
+    }
+}
