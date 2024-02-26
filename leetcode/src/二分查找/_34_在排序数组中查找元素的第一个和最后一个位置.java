34a37b5831b677d5c135b99646d50bd919b5d486
suxiang <2585546823@qq.com>
2024-02-26 15:42:19 +0800

    LeetCode HOT100: 二分查找

--- leetcode/src/二分查找/_34_在排序数组中查找元素的第一个和最后一个位置.java
+++ leetcode/src/二分查找/_34_在排序数组中查找元素的第一个和最后一个位置.java
@@ -39,4 +39,29 @@ public class _34_在排序数组中查找元素的第一个和最后一个位置
         res[1] = right;
         return res;
     }
+
+    public int[] searchRange2(int[] nums, int target) {
+        int[] res = new int[]{-1, -1};
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (left == nums.length || nums[left] != target) {
+            return res;
+        }
+        res[0] = left;
+        while (left < nums.length && nums[left] == target) {
+            left++;
+        }
+        res[1] = left - 1;
+        return res;
+    }
 }
