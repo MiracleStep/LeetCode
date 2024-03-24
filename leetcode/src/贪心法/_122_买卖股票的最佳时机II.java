2b497130c7f2b19c1b9cc2bbb0a4a511b26ea92b
suxiang <2585546823@qq.com>
2024-03-24 15:49:35 +0800

    LeetCode 面试经典150：贪心法

--- leetcode/src/贪心法/_122_买卖股票的最佳时机II.java
+++ leetcode/src/贪心法/_122_买卖股票的最佳时机II.java
@@ -1,5 +1,7 @@
 package 贪心法;
 
+import sun.applet.AppletClassLoader;
+
 public class _122_买卖股票的最佳时机II {
     public int maxProfit(int[] prices) {
         //贪心策略：只要今天股价比昨天高，就交易。
@@ -12,6 +14,7 @@ public class _122_买卖股票的最佳时机II {
                 res += prices[i] - prices[i - 1];
             }
         }
+
         return res;
     }
 }
