5c2c03196f75ddeb8cffc1cadf9dffb6c6eef350
suxiang <2585546823@qq.com>
2024-04-10 11:27:13 +0800

    剑指Offer：动态规划

---
+++ leetcode/src/字符串/LCR_020回文子串.java
@@ -0,0 +1,32 @@
+package 字符串;
+
+public class LCR_020回文子串 {
+
+    class Solution {
+        public int countSubstrings(String s) {
+            //dp表示以s[i],s[j]结尾的子串是否为回文串。
+            boolean[][] dp = new boolean[s.length()][s.length()];
+            int res = 0;
+            for(int i = 0; i < s.length();i++){
+                dp[i][i] = true;
+                res++;
+            }
+            for(int i = s.length() - 1; i >= 0; i--){
+                for(int j = i + 1; j < s.length(); j++){
+                    if(s.charAt(i) == s.charAt(j)){
+                        //是回文串
+                        if(j - i == 1){
+                            dp[i][j] = true;
+                        } else {
+                            dp[i][j] = dp[i+1][j-1];
+                        }
+                        if(dp[i][j] == true){
+                            res++;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
