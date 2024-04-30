509d57762a3fa0340cda7c2d7eddc8da829f095f
suxiang <2585546823@qq.com>
2024-04-30 13:44:23 +0800

    二叉树

--- leetcode/src/二叉树/_700_二叉搜索树中的搜索.java
+++ leetcode/src/二叉树/_700_二叉搜索树中的搜索.java
@@ -2,14 +2,12 @@ package 二叉树;
 
 public class _700_二叉搜索树中的搜索 {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null) return null;
-        if(root.val == val) return root;
-
-        TreeNode left = searchBST(root.left, val);
-        TreeNode right = searchBST(root.right, val);
-        if(left == null && right == null){
+        if (root == null) {
             return null;
         }
-        return left != null ? left : right;
+        if (val == root.val) {
+            return root;
+        }
+        return searchBST(val < root.val ? root.left : root.right, val);
     }
 }
