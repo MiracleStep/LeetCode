9a32c91378276d963a0c013fbb07a53d8a765a10
suxiang <2585546823@qq.com>
2024-05-04 16:09:59 +0800

    剑指Offer：单调栈

---
+++ leetcode/src/栈和队列/LCR_040_最大矩形.java
@@ -0,0 +1,87 @@
+package 栈和队列;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LCR_040_最大矩形 {
+    //动态规划不好搞，因为有后效性，需要确定子问题的解到底是哪几个方格的面积
+    //优化后的暴力解法
+    public int maximalRectangle(String[] matrix) {
+        int m = matrix.length;
+        if (m == 0) return 0;
+        int n = matrix[0].length();
+        int[][] left = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i].charAt(j) == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
+            }
+        }
+        int ret = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i].charAt(j) == '0') {
+                    continue;
+                }
+                int width = left[i][j];
+                int area = width;
+                for (int k = i - 1; k >= 0; k--) {
+                    width = Math.min(width, left[k][j]);
+                    area = Math.max(area, (i - k + 1) * width);
+                }
+                ret = Math.max(ret, area);
+            }
+        }
+        return ret;
+    }
+
+    //动态规划不好搞，因为有后效性，需要确定子问题的解到底是哪几个方格的面积
+    //维护一个单调递增的栈
+    public int maximalRectangle2(String[] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = matrix[0].length();
+        int[][] left = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i].charAt(j) == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        int ret = 0;
+        for (int j = 0; j < n; j++) { // 对于每一列，使用基于柱状图的方法
+            int[] up = new int[m];
+            int[] down = new int[m];
+
+            Deque<Integer> stack = new ArrayDeque<Integer>();
+            for (int i = 0; i < m; i++) {
+                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
+                    stack.pop();
+                }
+                up[i] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(i);
+            }
+            stack.clear();
+            for (int i = m - 1; i >= 0; i--) {
+                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
+                    stack.pop();
+                }
+                down[i] = stack.isEmpty() ? m : stack.peek();
+                stack.push(i);
+            }
+
+            for (int i = 0; i < m; i++) {
+                int height = down[i] - up[i] - 1;
+                int area = height * left[i][j];
+                ret = Math.max(ret, area);
+            }
+        }
+        return ret;
+    }
+}
