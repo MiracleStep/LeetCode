a197cfffcbba102594d899df73a0d0ad3dbf7703
suxiang <2585546823@qq.com>
2024-05-10 19:50:54 +0800

    二叉树

--- leetcode/src/二叉树/_669_修剪二叉搜索树.java
+++ leetcode/src/二叉树/_669_修剪二叉搜索树.java
@@ -1,16 +1,12 @@
 package 二叉树;
 
 public class _669_修剪二叉搜索树 {
+    //确定状态：修建二叉树，删除低于low的，和高于high的节点
     public TreeNode trimBST(TreeNode root, int low, int high) {
-        if(root == null) return null;
-
-        if(root.val < low){
-            return trimBST(root.right, low, high);
-        }
-        if(root.val > high){
-            return trimBST(root.left, low, high);
-        }
-        //root.val在这个区间
+        if (root == null) return null;
+        if (root.val < low) return trimBST(root.right, low, high);// 也就右边节点满足了
+        else if (root.val > high) return trimBST(root.left, low, high);//也就左边节点满足了
+        //low <= root.val <= high
         root.left = trimBST(root.left, low, high);
         root.right = trimBST(root.right, low, high);
         return root;
