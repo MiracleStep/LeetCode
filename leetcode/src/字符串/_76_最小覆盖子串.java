92201851a1f7fcb17e43640e998db77d3f09ea37
suxiang <2585546823@qq.com>
2024-05-22 10:44:40 +0800

    重刷HOT100:哈希表+滑动窗口。

--- leetcode/src/字符串/_76_最小覆盖子串.java
+++ leetcode/src/字符串/_76_最小覆盖子串.java
@@ -9,21 +9,22 @@ public class _76_最小覆盖子串 {
             if (++tCount[t.charAt(i) - 'A'] == 1) charCount++;
         }
         String res = "";
-        for (int i = 0, j = 0; i < s.length(); i++) {
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
             int ridx = s.charAt(i) - 'A'; //得到滑动窗口右边的字符索引
             sCount[ridx]++;
             if (sCount[ridx] == tCount[ridx]) {
                 charCount--;
             }
             if (charCount == 0) { //当前子串已经满足覆盖了
-                int lidx = s.charAt(j) - 'A';
-                while(j < i && sCount[lidx] > tCount[lidx]) { //左边存在冗余的字母，需要收缩滑动窗口
+                int lidx = s.charAt(left) - 'A';
+                while(left < i && sCount[lidx] > tCount[lidx]) { //左边存在冗余的字母，需要收缩滑动窗口
                     sCount[lidx]--;
-                    j++;
-                    lidx = s.charAt(j) - 'A'; //可能存在越界，需要j < i 判断一下
+                    left++;
+                    lidx = s.charAt(left) - 'A'; //可能存在越界，需要left < i 判断一下
                 }
-                if (res.length() == 0 || res.length() > i - j + 1) {
-                    res = s.substring(j, i + 1);
+                if (res.length() == 0 || res.length() > i - left + 1) {
+                    res = s.substring(left, i + 1);
                 }
             }
         }
