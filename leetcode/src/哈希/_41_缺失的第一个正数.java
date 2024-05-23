bbc9bad23deda12734ff582c83dbca61b69ebc30
suxiang <2585546823@qq.com>
2024-05-23 22:55:28 +0800

    重刷HOT100: 哈希表

--- leetcode/src/哈希/_41_缺失的第一个正数.java
+++ leetcode/src/哈希/_41_缺失的第一个正数.java
@@ -5,10 +5,13 @@ public class _41_缺失的第一个正数 {
         //原地哈希 哈希函数hash(nums[i]) = nums[i] - 1;
         for (int i = 0; i < nums.length; i++) {
             while (nums[i] > 0 && nums[i] - 1 < nums.length && nums[i] != nums[nums[i] - 1]) {
+                // 是正数、对应的hash值在哈希表的范围内、对应索引位置和当前值不同。
                 swap(nums, i, nums[i] - 1);
             }
+            //否则就继续下一个
         }
-        //重新遍历，找
+
+        //重新遍历，找第一个不满足条件的数。
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != i + 1) {
                 return i + 1;
@@ -17,7 +20,7 @@ public class _41_缺失的第一个正数 {
         return nums.length + 1;
     }
 
-    public void swap(int[] nums, int i, int j) {
+    private void swap(int[] nums, int i, int j) {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
