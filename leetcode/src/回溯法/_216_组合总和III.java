368c7e38611fc35f2c7eafb7eda307d4540061fa
suxiang <2585546823@qq.com>
2024-05-25 14:48:13 +0800

    回溯法

---
+++ leetcode/src/回溯法/_216_组合总和III.java
@@ -0,0 +1,34 @@
+package 回溯法;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class _216_组合总和III {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        if (k == 0) return res;
+        Deque<Integer> deque = new LinkedList<>();
+        dfs(k, n, 1, 0, deque, 0);
+        return res;
+    }
+
+    private void dfs(int k, int n, int begin, int sum, Deque<Integer> deque, int count) {
+        if (sum > n) {
+            //剪枝
+            return;
+        }
+        if (count == k && sum == n) {
+            res.add(new ArrayList<>(deque));
+            return;
+        }
+
+        for (int i = begin; i <= 9; i++) {
+            deque.offer(i);
+            dfs(k, n, i + 1, sum + i, deque, count + 1);
+            deque.pollLast();
+        }
+    }
+}
