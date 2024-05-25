1f589d2678547b2e521ad5cfee11ec649eeb2097
suxiang <2585546823@qq.com>
2024-05-25 14:08:12 +0800

    重刷HOT100: 数组

--- leetcode/src/数组/_240_搜索二维矩阵II.java
+++ leetcode/src/数组/_240_搜索二维矩阵II.java
@@ -2,14 +2,17 @@ package 数组;
 
 public class _240_搜索二维矩阵II {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i = matrix.length - 1, j = 0;
-        while (i >= 0 && j < matrix[0].length) {
-            if (matrix[i][j] > target) {
-                i--;
-            } else if (matrix[i][j] < target) {
-                j++;
-            } else {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int x = 0, y = n - 1;
+        //从右上角进行查找
+        while (x < m && y >= 0) {
+            if (target == matrix[x][y]) {
                 return true;
+            } else if (matrix[x][y] < target) {
+                x++;
+            } else {
+                y--;
             }
         }
         return false;
