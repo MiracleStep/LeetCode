21db4d536ab3d762268bfaffe73eee8f91a5b8a7
suxiang <2585546823@qq.com>
2024-05-27 22:57:08 +0800

    重刷HOT100: 链表

--- 00-Test/src/Test.java
+++ 00-Test/src/Test.java
@@ -3,13 +3,6 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws ParseException {
-        Scanner sc = new Scanner(System.in);
-        int k = Integer.parseInt(sc.nextLine());
-        String s = sc.nextLine();
-        int len = s.length();
-        StringBuilder sb = new StringBuilder();
-        sb.append(s.substring(len - k, len));
-        sb.append(s.substring(0, len - k));
-        System.out.println(sb.toString());
+
     }
 }
