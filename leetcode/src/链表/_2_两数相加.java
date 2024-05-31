55576dd38bb59a3e4b697238ed66a840e3ac1078
suxiang <2585546823@qq.com>
2024-05-31 10:39:58 +0800

    重刷HOT100: 链表

--- leetcode/src/链表/_2_两数相加.java
+++ leetcode/src/链表/_2_两数相加.java
@@ -2,39 +2,25 @@ package 链表;
 
 public class _2_两数相加 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return new ListNode(0);
-        }
-        ListNode cur = new ListNode(0);
-        ListNode head = cur; //固定头结点
-        int sum = 0; //保存加和和进位
-        while (l1 != null && l2 != null) {
-            sum += l1.val + l2.val;
-            cur.next = new ListNode(sum % 10);
-            sum /= 10;
-            l1 = l1.next;
-            l2 = l2.next;
+        ListNode dummyNode = new ListNode();
+        ListNode cur = dummyNode;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            cur.next = new ListNode(carry % 10);
+            carry /= 10;
             cur = cur.next;
         }
-        while (l1 != null) {
-            sum += l1.val;
-            l1.val = sum % 10;
-            cur.next = l1;
-            sum /= 10;
-            cur = cur.next;
-            l1 = l1.next;
+        if (carry != 0) {
+            cur.next = new ListNode(carry);
         }
-        while (l2 != null) {
-            sum += l2.val;
-            l2.val = sum % 10;
-            cur.next = l2;
-            sum /= 10;
-            cur = cur.next;
-            l2 = l2.next;
-        }
-        if (sum != 0) { //最后可能还有进位
-            cur.next = new ListNode(sum);
-        }
-        return head.next;
+        return dummyNode.next;
     }
 }
