205f31d32cb6522235e3c3ee546920d6c9d6f995
suxiang <2585546823@qq.com>
2024-06-03 19:45:01 +0800

    重刷HOT100: 链表

--- leetcode/src/链表/_138_随机链表的复制.java
+++ leetcode/src/链表/_138_随机链表的复制.java
@@ -6,12 +6,13 @@ import java.util.Map;
 public class _138_随机链表的复制 {
     public Node copyRandomList(Node head) {
         if (head == null) return null;
-        Map<Node, Node> map = new HashMap<>();//原链表和新链表的关系映射。
+        Map<Node, Node> map = new HashMap<>();//哈希表
         Node cur = head;
         while (cur != null) {
             map.put(cur, new Node(cur.val));
             cur = cur.next;
         }
+
         cur = head;
         while (cur != null) {
             map.get(cur).next = map.get(cur.next);
