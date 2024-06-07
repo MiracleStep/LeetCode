fcd2da4d9e982c48fa4d1343b0c1fe414342503b
suxiang <2585546823@qq.com>
2024-06-07 16:55:45 +0800

    重刷HOT100: 二叉树

--- leetcode/src/二叉树/_108_将有序数组转换为二叉搜索树.java
+++ leetcode/src/二叉树/_108_将有序数组转换为二叉搜索树.java
@@ -2,16 +2,16 @@ package 二叉树;
 
 public class _108_将有序数组转换为二叉搜索树 {
     public TreeNode sortedArrayToBST(int[] nums) {
-        if (nums == null) return null;
-        return binaryArrayToBST(nums, 0, nums.length - 1);
+        if (nums.length == 0) return null;
+        return sortedArrayToBST(nums, 0, nums.length);
     }
 
-    private TreeNode binaryArrayToBST(int[] nums, int left, int right) {
-        if (left > right) return null;
-        int mid = left + (right - left) / 2;
-        TreeNode root = new TreeNode(nums[mid]);
-        root.left = binaryArrayToBST(nums, left, mid - 1);
-        root.right = binaryArrayToBST(nums, mid + 1, right);
-        return root;
+    private TreeNode sortedArrayToBST(int[] nums, int begin, int end) {
+        if (begin >= end) return null;
+        int mid = begin + (end - begin) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBST(nums, begin, mid);
+        node.right = sortedArrayToBST(nums, mid + 1, end);
+        return node;
     }
 }
