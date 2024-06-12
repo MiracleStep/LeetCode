2b156e2e55cc17df746821cc2b0c3a1dbf4b7b08
suxiang <2585546823@qq.com>
2024-06-12 23:06:05 +0800

    重刷HOT100: 二叉树

--- leetcode/src/二叉树/_199_二叉树的右视图.java
+++ leetcode/src/二叉树/_199_二叉树的右视图.java
@@ -6,29 +6,30 @@ import java.util.List;
 import java.util.Queue;
 
 public class _199_二叉树的右视图 {
-    //BFS：层次遍历。
+    //BFS:层次遍历，从右往左遍历，第一个就是右视图
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
+        if (root == null) {
+            return res;
+        }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         while (!queue.isEmpty()) {
+            res.add(queue.peek().val);
             int size = queue.size();
-            TreeNode poll = null;
-            while (size > 0) {
-                poll = queue.poll();
-                if (poll.left != null) {
-                    queue.offer(poll.left);
-                }
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
                 if (poll.right != null) {
                     queue.offer(poll.right);
                 }
-                size--;
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
             }
-            res.add(poll.val);
         }
         return res;
     }
+
     //DFS
     List<Integer> res = new ArrayList<>();
 
@@ -39,13 +40,10 @@ public class _199_二叉树的右视图 {
 
     private void dfs(TreeNode root, int depth) {
         if (root == null) return;
-
         if (res.size() == depth) {
             res.add(root.val);
         }
-
-        depth++;
-        dfs(root.right, depth);
-        dfs(root.left, depth);
+        dfs(root.right, depth + 1);
+        dfs(root.left, depth + 1);
     }
 }
