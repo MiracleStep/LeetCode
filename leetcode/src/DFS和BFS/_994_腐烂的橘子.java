5b668d50a488734c28a6103a6abe41b9d66efc47
suxiang <2585546823@qq.com>
2024-06-16 12:25:47 +0800

    重刷HOT100: DFS/BFS

--- leetcode/src/DFS和BFS/_994_腐烂的橘子.java
+++ leetcode/src/DFS和BFS/_994_腐烂的橘子.java
@@ -5,10 +5,10 @@ import java.util.Queue;
 
 public class _994_腐烂的橘子 {
     //BFS
+    int dir[][] = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
     public int orangesRotting(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        if (m == 0 || n == 0) return -1;
-        int dir[][] = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        int m = grid.length;
+        int n = grid[0].length;
         Queue<int[]> queue = new LinkedList<>();
         int count = 0; //统计好橘子的数量
         for (int i = 0; i < m; i++) {
@@ -20,21 +20,24 @@ public class _994_腐烂的橘子 {
                 }
             }
         }
+        //BFS
         int res = 0;
         while (count > 0 && !queue.isEmpty()) {
-            int size = queue.size();
             res++;
+            int size = queue.size();
             while (size-- > 0) {
                 int[] poll = queue.poll();
                 int x = poll[0];
                 int y = poll[1];
+
                 for (int i = 0; i < 4; i++) {
                     int nx = x + dir[i][0];
                     int ny = y + dir[i][1];
+
                     if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 1) {
                         //没有越界并且是新鲜橘子
                         grid[nx][ny] = 2;//变成烂橘子
-                        count--;//好橘子数量-1
+                        count--;
                         queue.offer(new int[]{nx, ny});
                     }
                 }
