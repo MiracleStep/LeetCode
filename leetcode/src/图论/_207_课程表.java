a2d738caa3931246e7ac6095ab5f04ade05079bb
suxiang <2585546823@qq.com>
2024-06-17 12:14:35 +0800

    重刷HOT100: 图论

--- leetcode/src/图论/_207_课程表.java
+++ leetcode/src/图论/_207_课程表.java
@@ -44,23 +44,30 @@ public class _207_课程表 {
     }
 
     public boolean canFinish2(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adjacency = new ArrayList<>(); //邻接矩阵
+        List<List<Integer>> adjacency = new ArrayList<>(); //邻接表
         for (int i = 0; i < numCourses; i++)
-            adjacency.add(new ArrayList<>()); //邻接矩阵初始化
-        int[] flags = new int[numCourses]; // 1表示当前递归已经访问过了，-1表示之前递归访问过了，0表示未访问过。
+            adjacency.add(new ArrayList<>()); //邻接表初始化
+        //建立邻接表
         for (int[] cp : prerequisites)
-            adjacency.get(cp[1]).add(cp[0]); //建立邻接矩阵
-        for (int i = 0; i < numCourses; i++)
-            if (!dfs(adjacency, flags, i)) return false; //依次对每个节点进行深度优先遍历直到DFS遇到自己已经DFS过的节点
+            adjacency.get(cp[1]).add(cp[0]);
+        // 1表示当前递归已经访问过了，-1表示之前递归访问过了，0表示未访问过。
+        int[] flags = new int[numCourses];
+        //依次对每个节点进行深度优先遍历直到DFS遇到自己已经DFS过的节点
+        for (int i = 0; i < numCourses; i++)  {
+            if (!dfs(adjacency, flags, i)) return false;
+        }
         return true;
     }
 
     private boolean dfs(List<List<Integer>> adjacency, int[] flags, int i) {
         if (flags[i] == 1) return false; // 当前递归已经访问过了，出现环。
         if (flags[i] == -1) return true; //之前递归已经访问过了，无需访问。
-        flags[i] = 1;
-        for (Integer j : adjacency.get(i))
-            if (!dfs(adjacency, flags, j)) return false;//DFS i的每一个出度节点。
+
+        flags[i] = 1;  //标记当前节点正在遍历
+        //DFS i的每一个出度节点。
+        for (Integer j : adjacency.get(i)) {
+            if (!dfs(adjacency, flags, j)) return false;
+        }
         flags[i] = -1; //回溯，当前设置节点被其他节点遍历过了。
         return true;
     }
