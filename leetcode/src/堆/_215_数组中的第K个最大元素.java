5c174867fb25379b3597a5ac6a6902d48f86d952
suxiang <2585546823@qq.com>
2024-07-24 14:25:37 +0800

    重刷HOT100: 堆

--- leetcode/src/堆/_215_数组中的第K个最大元素.java
+++ leetcode/src/堆/_215_数组中的第K个最大元素.java
@@ -3,14 +3,52 @@ package 堆;
 import java.util.PriorityQueue;
 
 public class _215_数组中的第K个最大元素 {
+    //堆 最简单，但面试不要写。
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();//维护容量为k的小跟队
-        for (int num : nums) {
-            heap.offer(num);
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int n : nums) {
+            heap.offer(n);
             if (heap.size() > k) {
-                heap.poll();//如果大于k，把最小的poll掉
+                heap.poll();
             }
         }
         return heap.peek();
+    }
+
+    //快速排序
+    public int findKthLargest2(int[] nums, int k) {
+        return quickSort(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    //结束条件就是到达倒数第k个，因为根据快排的规定，选择的轴点元素位置不会改变，只会继续对左右元素进行排序
+    private int quickSort(int[] nums, int begin, int end, int k) {
+        if (begin == end) return nums[begin];
+        int pivot = nums[begin], left = begin, right = end;
+        while (left < right) {
+            while (left < right) {
+                if (nums[right] >= pivot) {
+                    right--;
+                } else {
+                    nums[left++] = nums[right];
+                    break;
+                }
+            }
+            while (left < right) {
+                if (nums[left] <= pivot) {
+                    left++;
+                } else {
+                    nums[right--] = nums[left];
+                    break;
+                }
+            }
+        }
+        nums[left] = pivot;
+        //轴点元素位置
+        if (left == k) return nums[left];
+        if (left > k) {
+            return quickSort(nums, begin, left - 1, k);
+        } else {
+            return quickSort(nums, left + 1, end, k);
+        }
     }
 }
--- leetcode/src/堆/_347_前K个高频元素.java
+++ leetcode/src/堆/_347_前K个高频元素.java
@@ -1,55 +1,30 @@
 package 堆;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class _347_前K个高频元素 {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> {
+            return map.get(a) - map.get(b);
+        });
 
-    //堆 最简单，但面试不要写。
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        for (int n : nums) {
-            heap.offer(n);
+        for (int key : map.keySet()) {
+            heap.offer(key);
             if (heap.size() > k) {
                 heap.poll();
             }
         }
-        return heap.peek();
-    }
 
-    //快速排序
-    public int findKthLargest2(int[] nums, int k) {
-        return quickSort(nums, 0, nums.length - 1, nums.length - k);
-    }
-
-    //结束条件就是到达倒数第k个，因为根据快排的规定，选择的轴点元素位置不会改变，只会继续对左右元素进行排序
-    private int quickSort(int[] nums, int begin, int end, int k) {
-        if (begin == end) return nums[begin];
-        int pivot = nums[begin], left = begin, right = end;
-        while (left < right) {
-            while (left < right) {
-                if (nums[right] >= pivot) {
-                    right--;
-                } else {
-                    nums[left++] = nums[right];
-                    break;
-                }
-            }
-            while (left < right) {
-                if (nums[left] <= pivot) {
-                    left++;
-                } else {
-                    nums[right--] = nums[left];
-                    break;
-                }
-            }
+        int[] res = new int[heap.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = heap.poll();
         }
-        nums[left] = pivot;
-        //轴点元素位置
-        if (left == k) return nums[left];
-        if (left > k) {
-            return quickSort(nums, begin, left - 1, k);
-        } else {
-            return quickSort(nums, left + 1, end, k);
-        }
+        return res;
     }
 }
