9ed8fc3b9bdb40712ee2e23be208705708f6af1e
suxiang <2585546823@qq.com>
2024-07-27 14:38:49 +0800

    重刷HOT100: 贪心+动态规划

--- leetcode/src/动态规划/_45_跳跃游戏II.java
+++ leetcode/src/动态规划/_45_跳跃游戏II.java
@@ -1,12 +1,25 @@
 package 动态规划;
 
 public class _45_跳跃游戏II {
-    //动态规划
+    //贪心算法
     public int jump(int[] nums) {
-        //确定状态：定义dp[i]为到达i位置所需要的最小步数 。
+        int maxCoverage = 0;
+        int endPoint = 0;
+        int steps = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxCoverage = Math.max(nums[i] + i, maxCoverage);
+            if (i == endPoint) {
+                endPoint = maxCoverage;
+                steps++;
+            }
+        }
+        return steps;
+    }
+    //动态规划
+    public int jump2(int[] nums) {
+        //确定状态：定义dp[i]为到达第 i 个位置所需要的最小步数 。
         int[] dp = new int[nums.length];
         //确定初始状态
-
         dp[0] = 0;
         //确定状态转移方程
         int j = 0;//记录到达i位置的最远的点
@@ -16,26 +29,6 @@ public class _45_跳跃游戏II {
             }
             //dp[j] <= dp[j+1]是肯定成立的，所以只要找到可以到达dp[i]的第一个dp[j]即可。
             dp[i] = dp[j] + 1;
-        }
-        return dp[nums.length - 1];
-    }
-
-    //动态规划，时间复杂度高
-    public int jump2(int[] nums) {
-        //确定状态：定义dp[i]为到达第 i 个位置所需要的最小步数 。
-        int[] dp = new int[nums.length];
-        //确定初始状态
-        dp[0] = 0;
-        //确定状态转移方程
-        for(int i = 1; i < nums.length; i++){
-            int minStep = Integer.MAX_VALUE;
-            for(int j = 0; j < i; j++){
-                if(j + nums[j] >= i){
-                    //j处能够到达i处
-                    minStep = Math.min(minStep, dp[j]);
-                }
-            }
-            dp[i] = minStep + 1;
         }
         return dp[nums.length - 1];
     }
