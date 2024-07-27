8c7886cddf0b74ebd491c2daa2897d40daf7f0e7
suxiang <2585546823@qq.com>
2024-07-27 14:44:20 +0800

    重刷HOT100: 贪心算法

--- leetcode/src/贪心法/_763_划分字母区间.java
+++ leetcode/src/贪心法/_763_划分字母区间.java
@@ -6,18 +6,20 @@ import java.util.List;
 public class _763_划分字母区间 {
     //贪心法：通过局部寻找每个片段可能的最小结束下标，达到全局每个片段的长度一定是符合要求的最短长度。
     public List<Integer> partitionLabels(String s) {
-        int[] last = new int[26];//哈希表
-        for (int i = 0; i < s.length(); i++) {
-            last[s.charAt(i) - 'a'] = i;//找到当前字符的最后一个下标
-        }
+        int n = s.length();
+        char[] chars = s.toCharArray();
         List<Integer> res = new ArrayList<>();
-        int left = 0;
-        int end = 0;//记录当前片段结束的下标
-        for (int i = 0; i < s.length(); i++) {
-            end = Math.max(last[s.charAt(i) - 'a'], end);//遇到新字符就更新结束的下标
+        int[] hash = new int[26];//hash表
+        for (int i = 0; i < n; i++) {
+            hash[chars[i] - 'a'] = i;
+        }
+        int begin = 0, end = 0;
+        for (int i = 0; i < n; i++) {
+            end = Math.max(hash[chars[i]- 'a'], end);
             if (i == end) {
-                res.add(i - left + 1);
-                left = end + 1;
+                res.add(end - begin + 1);
+                end += 1;
+                begin = end;
             }
         }
         return res;
