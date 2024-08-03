98e3520b8803e0259fe9b0ae8493ade1ce05e6d3
suxiang <2585546823@qq.com>
2024-08-03 20:33:38 +0800

    重刷HOT100: 堆

--- leetcode/src/堆/_295_数据流的中位数.java
+++ leetcode/src/堆/_295_数据流的中位数.java
@@ -6,15 +6,15 @@ public class _295_数据流的中位数 {
     PriorityQueue<Integer> A, B;
     //建立一个 小顶堆 A 和 大顶堆 B，各保存列表的一半元素
     public _295_数据流的中位数() {
-        A = new PriorityQueue<>();
-        B = new PriorityQueue<>((x, y) -> (y - x));
+        A = new PriorityQueue<>();  //存储较大的那一半元素
+        B = new PriorityQueue<>((x, y) -> (y - x));  //存储较小的那一半元素
     }
 
     public void addNum(int num) {
-        if (A.size() == B.size()) {
+        if (A.size() == B.size()) {  //向A添加元素，因为num可能是较小的元素，先存储过滤排序一下。
             B.offer(num);
             A.offer(B.poll());
-        } else {
+        } else {  //向B添加元素
             A.offer(num);
             B.offer(A.poll());
         }
