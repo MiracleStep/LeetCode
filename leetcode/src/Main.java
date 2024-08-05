f7156e5898a3a7f02dcbe52f4bfa6a2a0ae62461
suxiang <2585546823@qq.com>
2024-08-05 23:47:00 +0800

    重刷HOT100: 动态规划

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,4 +1,9 @@
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid.toString().length());
+
     }
 }
