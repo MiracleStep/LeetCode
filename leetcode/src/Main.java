689df26f397bac67a2cc06dc5dd87ace46780aef
suxiang <2585546823@qq.com>
2024-08-08 23:38:58 +0800

    无

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,9 +1,5 @@
-import java.util.UUID;
-
 public class Main {
     public static void main(String[] args) {
-        UUID uuid = UUID.randomUUID();
-        System.out.println(uuid.toString().length());
 
     }
 }
