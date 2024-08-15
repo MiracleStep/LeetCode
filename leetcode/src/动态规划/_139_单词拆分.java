e7790f77c4e2cfcea0b38e3f6d09d7bec63761cd
suxiang <2585546823@qq.com>
2024-08-15 23:32:13 +0800

    重刷LeetCode HOT100：动态规划

--- leetcode/src/动态规划/_139_单词拆分.java
+++ leetcode/src/动态规划/_139_单词拆分.java
@@ -6,8 +6,9 @@ public class _139_单词拆分 {
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length()];//以字符串i结尾是否可以拼接
         if (s.length() == 1) {
-            return wordDict.contains(s.substring(0, 1));
+            return  wordDict.contains(s.substring(0, 1));
         }
+        //初始化
         dp[0] = wordDict.contains(s.substring(0, 1));
         for (int i = 1; i < s.length(); i++) {
             if (wordDict.contains(s.substring(0, i + 1))) {
