4c7cfac4d11e1002765b3d1a078430d6a5842c0c
suxiang <2585546823@qq.com>
2024-08-19 22:54:01 +0800

    重刷LeetCode HOT100：动态规划

--- leetcode/src/动态规划/_139_单词拆分.java
+++ leetcode/src/动态规划/_139_单词拆分.java
@@ -3,7 +3,6 @@ package 动态规划;
 import java.util.List;
 
 public class _139_单词拆分 {
-
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length()];//以字符串i结尾是否可以拼接
         if (s.length() == 1) {
@@ -24,5 +23,23 @@ public class _139_单词拆分 {
             }
         }
         return dp[s.length() - 1];
+    }
+
+
+    //解法2：时间复杂度更优秀
+    public boolean wordBreak2(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i < dp.length; i++) {
+            for (String word : wordDict) {
+                int sz = word.length();
+                // 检查子串是否匹配，并确保不会超出字符串的边界
+                if (i - sz >= 0 && s.substring(i - sz, i).equals(word)) {
+                    dp[i] = dp[i - sz];
+                    if (dp[i]) break;
+                }
+            }
+        }
+        return dp[s.length()];
     }
 }
--- leetcode/src/动态规划/_300_最长递增子序列.java
+++ leetcode/src/动态规划/_300_最长递增子序列.java
@@ -17,4 +17,33 @@ public class _300_最长递增子序列 {
         }
         return res;
     }
+
+    //时间复杂度最优
+    public int lengthOfLIS2(int[] nums) {
+        //tails[i]表示长度为i + 1的子序列尾部的最小元素
+        int[] tails = new int[nums.length];
+        int len = 0;
+        tails[len++] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > tails[len - 1]) {
+                //大于数组最后一个元素
+                tails[len++] = nums[i];
+            } else if (nums[i] < tails[len - 1]) {
+                //小于数组最后一个元素，那肯定可以替换掉数组中某个元素
+                //采用二分查找
+                int left = 0;
+                int right = len - 1;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    if (tails[mid] < nums[i]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+                tails[left] = nums[i];
+            }
+        }
+        return len;
+    }
 }
