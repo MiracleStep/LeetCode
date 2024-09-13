45567a8b41ba7528fc72f86e1f6d9e072c0f7a33
suxiang <2585546823@qq.com>
2024-09-13 09:58:41 +0800

    重刷LeetCode HOT100：动态规划

--- leetcode/src/动态规划/_64_最小路径和.java
+++ leetcode/src/动态规划/_64_最小路径和.java
@@ -24,4 +24,23 @@ public class _64_最小路径和 {
         }
         return dp[m-1][n-1];
     }
+
+    public int minPathSum2(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        //到达:i,j位置时的路径上数字总和最小的值
+        int[] dp = new int[n];
+        //初始化
+        dp[0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = grid[0][i] + dp[i-1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            dp[0] = dp[0] + grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = grid[i][j] + Math.min(dp[j - 1], dp[j]);
+            }
+        }
+        return dp[n - 1];
+    }
 }
