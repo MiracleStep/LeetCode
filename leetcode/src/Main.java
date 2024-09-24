3ebebac47706fbfe328daca3e7eba1673f5979b4
suxiang <2585546823@qq.com>
2024-09-24 22:19:18 +0800

    无

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println();
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            ArrayList<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        Set<Map.Entry<String, ArrayList<String>>> entries = map.entrySet();
+        return new ArrayList<List<String>>(map.values());
     }
 }
