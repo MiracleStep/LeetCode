3e363b73d6249589f6fabc275e15963df50f1cfc
suxiang <2585546823@qq.com>
2024-09-26 13:07:28 +0800

    重刷LeetCode HOT100

--- leetcode/src/链表/_141_环形链表.java
+++ leetcode/src/链表/_141_环形链表.java
@@ -15,4 +15,19 @@ public class _141_环形链表 {
         }
         return false;
     }
+
+    public boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
