7782c40a282fe76f55674026862f5e849069fdc7
suxiang <2585546823@qq.com>
2024-09-27 11:12:55 +0800

    重刷LeetCode HOT100

--- leetcode/src/二叉树/_114_二叉树展开为链表.java
+++ leetcode/src/二叉树/_114_二叉树展开为链表.java
@@ -1,38 +1,17 @@
 package 二叉树;
 
 public class _114_二叉树展开为链表 {
-    //解法1
     public void flatten(TreeNode root) {
         if (root == null) return;
         flatten(root.left);
-        flatten(root.right);
-        if (root.left != null) {
-            TreeNode cur = root.left;
-            while (cur.right != null) {
-                cur = cur.right;
-            }
-            //左节点cur遍历到头了
-            cur.right = root.right;
-            root.right = root.left;
-            root.left = null;
-        }
-    }
-    //解法2
-    public void flatten2(TreeNode root) {
-        dfs(root);
-    }
-
-    public TreeNode dfs(TreeNode root) {
-        if (root == null) return null;
         TreeNode tmp = root.right;
-        root.right = dfs(root.left);
+        root.right = root.left;
         root.left = null;
         TreeNode cur = root;
         while (cur.right != null) {
             cur = cur.right;
         }
-
-        cur.right = dfs(tmp);
-        return root;
+        flatten(tmp);
+        cur.right = tmp;
     }
 }
