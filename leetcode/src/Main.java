633be4c456f3d13e7b97e274a92e9f2350609430
suxiang <2585546823@qq.com>
2024-10-02 23:42:13 +0800

    无

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,9 +1,5 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Integer> list = new LinkedList<>();
-        list.add(0);
-        System.out.println(list.get(0));
-    }
+
 }
