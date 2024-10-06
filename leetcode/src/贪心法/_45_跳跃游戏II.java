b18b88cbb82f4934aab244da3e92d170139d6913
suxiang <2585546823@qq.com>
2024-10-06 22:35:19 +0800

    重刷LeetCode HOT100

--- leetcode/src/贪心法/_45_跳跃游戏II.java
+++ leetcode/src/贪心法/_45_跳跃游戏II.java
@@ -1,20 +1,18 @@
 package 贪心法;
 
 public class _45_跳跃游戏II {
-    //贪心法
     public int jump(int[] nums) {
-        int curCoverage = 0;
-        int nextCoverage = 0;
         int res = 0;
-        for(int i = 0;i < nums.length - 1; i++){
-            //记录从当前节点跳到下一个节点的最大范围。
-            nextCoverage = Math.max(nextCoverage, i + nums[i]);
-            if(curCoverage >= nums.length - 1){
-                return res;
-            }
-            if(i == curCoverage){//到达覆盖范围的最后一个节点
+        int maxCoverage = 0;
+        int endPoint = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxCoverage = Math.max(maxCoverage, i + nums[i]);
+            if (i == endPoint) {
                 res++;
-                curCoverage = nextCoverage;
+                endPoint = maxCoverage;
+                if (endPoint >= nums.length - 1) {//剪枝：判断是否能够提前结束
+                    return res;
+                }
             }
         }
         return res;
