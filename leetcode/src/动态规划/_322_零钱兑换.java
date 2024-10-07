ad8d00e371f9e36149320d3df4eea244381f7b6d
suxiang <2585546823@qq.com>
2024-10-07 23:51:30 +0800

    重刷LeetCode HOT100

--- leetcode/src/动态规划/_322_零钱兑换.java
+++ leetcode/src/动态规划/_322_零钱兑换.java
@@ -1,23 +1,21 @@
 package 动态规划;
 
+import java.util.Arrays;
+
 public class _322_零钱兑换 {
     public int coinChange(int[] coins, int amount) {
-        //dp表示凑到i元的最小硬币数
         int[] dp = new int[amount + 1];
-        dp[0] = 0;
-        for(int i = 1; i <= amount; i++){
-            int min = Integer.MAX_VALUE;
-            for(int coin : coins){
-                if(coin <= i && dp[i - coin] != -1 && dp[i - coin] < min){
-                    min = dp[i - coin];
+        Arrays.sort(coins);
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (coin > i) {//面额太大了，无法找。直接剪枝
+                    continue;
+                } else if (dp[i - coin] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
-            if(min == Integer.MAX_VALUE){
-                dp[i] = -1; // 找不开
-            } else {
-                dp[i] = min + 1; // 找的开。
-            }
         }
-        return dp[amount];
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
