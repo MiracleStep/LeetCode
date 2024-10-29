16d2abaec92725bd16a04ac05d37301dbf0576ea
suxiang <2585546823@qq.com>
2024-10-29 20:36:21 +0800

    无

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,3 +1,10 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        ArrayList<Object> objects = new ArrayList<>(objectObjectHashMap.values());
+
+    }
 }
