f252c855d69e996725620bf2ec292f3d2371611e
Mirac <2585546823@qq.com>
2024-11-14 23:30:23 +0800

    重刷Hot100：day02

--- leetcode/src/数组/_240_搜索二维矩阵II.java
+++ leetcode/src/数组/_240_搜索二维矩阵II.java
@@ -2,19 +2,19 @@ package 数组;
 
 public class _240_搜索二维矩阵II {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int x = 0, y = n - 1;
-        //从右上角进行查找
-        while (x < m && y >= 0) {
-            if (target == matrix[x][y]) {
-                return true;
-            } else if (matrix[x][y] < target) {
-                x++;
+        int m = matrix.length, n = matrix[0].length;
+        int row = 0, col = n - 1;
+        while (true) {
+            if (row < 0 || row >= m || col < 0 || col >= n) {
+                return false;
+            }
+            if (matrix[row][col] > target) {
+                col--;
+            } else if (matrix[row][col] < target) {
+                row++;
             } else {
-                y--;
+                return true;
             }
         }
-        return false;
     }
 }
