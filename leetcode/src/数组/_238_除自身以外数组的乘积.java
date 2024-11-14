095c78cb86a254b4201c99494cfa2149d5b7c3da
Mirac <2585546823@qq.com>
2024-11-14 19:01:20 +0800

    重刷Hot100：day02

--- leetcode/src/数组/_238_除自身以外数组的乘积.java
+++ leetcode/src/数组/_238_除自身以外数组的乘积.java
@@ -2,22 +2,21 @@ package 数组;
 
 public class _238_除自身以外数组的乘积 {
     public int[] productExceptSelf(int[] nums) {
-        int len = nums.length;
-        int[] answer = new int[nums.length];
-        //前缀乘积
-        int[] pre = new int[len];
+        int n = nums.length;
+        //前缀乘积，只需要定义len即可
+        int[] pre = new int[n];
         //后缀乘积
-        int[] post = new int[len];
-        pre[0] = post[len - 1] = 1;
+        int[] post = new int[n];
+        pre[0] = post[n - 1] = 1;
         //计算前缀和后缀乘积
-        for (int i = 1; i < nums.length; i++) {
-            pre[i] = nums[i - 1] * pre[i - 1];
-            post[len - i - 1] = post[len - i] * nums[len - i];
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
+            post[n - i - 1] = post[n - i] * nums[n - i];
         }
-
-        for (int i = 0; i < len; i++) {
-            answer[i] = pre[i] * post[i];
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = pre[i] * post[i];
         }
-        return answer;
+        return res;
     }
 }
