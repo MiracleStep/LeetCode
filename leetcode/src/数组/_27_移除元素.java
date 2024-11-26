eb10fc9e7c6872d0d14bb6982ef6604362943b37
Mirac <2585546823@qq.com>
2024-11-26 23:47:42 +0800

    重刷Hot100

--- leetcode/src/数组/_27_移除元素.java
+++ leetcode/src/数组/_27_移除元素.java
@@ -1,11 +1,6 @@
 package 数组;
 
 public class _27_移除元素 {
-    public static void main(String[] args) {
-        int[] a = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
-        int i = new _27_移除元素().removeElement(a, 2);
-        System.out.println(i);
-    }
 
     //快慢指针法:相当于fast在喂食，每当喂slow一口值，slow就向前走一步。而fast则是往前找不背丢掉的食物。
     public int removeElement(int[] nums, int val) {
