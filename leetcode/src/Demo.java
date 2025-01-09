e5e9e560ceff82660406d21d99c4cdbf061118bc
suxiang <2585546823@qq.com>
2025-01-09 23:25:56 +0800

    重刷Hot100

--- leetcode/src/Demo.java
+++ leetcode/src/Demo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author Mirac
  * @date 8/1/2025
@@ -7,34 +9,9 @@ public class Demo {
     static Object resource2 = new Object();
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(()->{
-            synchronized (resource1) {
-                System.out.println("Thread1 running");
-                try {
-                    Thread.sleep(100);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                synchronized (resource2) {
-                    System.out.println("get resource2");
-                }
-            }
-        });
+        Scanner sc = new Scanner(System.in);
+        String next = sc.next();
+        char[] charArray = next.toCharArray();
 
-        Thread t2 = new Thread(()-> {
-            synchronized (resource2) {
-                System.out.println("Thread2 running");
-                try {
-                    Thread.sleep(100);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                synchronized (resource1) {
-                    System.out.println("get resource1");
-                }
-            }
-        });
-        t1.start();
-        t2.start();
     }
 }
