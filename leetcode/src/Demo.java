ee6a802261e340706c8f34e919091cb2b491c169
suxiang <2585546823@qq.com>
2025-02-05 18:57:06 +0800

    重刷Hot100

--- leetcode/src/Demo.java
+++ leetcode/src/Demo.java
@@ -1,17 +1,47 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  * @author Mirac
  * @date 8/1/2025
  */
 public class Demo {
-    static Object resource1 = new Object();
-    static Object resource2 = new Object();
-
+    static int[] arrays = new int[]{50, 21, 80, 43, 38, 14, 56};
+    static int heapSize = arrays.length;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String next = sc.next();
-        char[] charArray = next.toCharArray();
+        for (int index = (heapSize >> 1) - 1; index >= 0; index--) {
+            siftDown(index);
+        }
+        int k = 4;
+        while (k-- > 0) {
+            swap(0, --heapSize);
+            siftDown(0);
+        }
+        System.out.println(Arrays.toString(arrays));
+    }
 
+    private static void swap(int i, int i1) {
+        int tmp = arrays[i];
+        arrays[i] = arrays[i1];
+        arrays[i1] = tmp;
+    }
+
+    private static void siftDown(int index) {
+        int cur = arrays[index];
+        int half = heapSize >> 1;
+        while (index < half) {
+            int childIndex = (index << 1) + 1;
+            int child = arrays[childIndex];
+            if (childIndex + 1 < heapSize && arrays[childIndex + 1] < child) {
+                childIndex = childIndex + 1;
+                child = arrays[childIndex];
+            }
+            if (child < cur) {
+                arrays[index] = child;
+                index = childIndex;
+            } else {
+                break;
+            }
+        }
+        arrays[index] = cur;
     }
 }
--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,3 +1,11 @@
-public class Main {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+
+        }
+    }
 }
