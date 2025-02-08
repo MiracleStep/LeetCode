2d618f952d3c6ba740df4258515db7bec371ee1c
suxiang <2585546823@qq.com>
2025-02-08 16:35:05 +0800

    Hot150

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,11 +1,14 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        double x = 2.0;
+        int n = 5;
 
+        double ans = 1.0;
+        while (n > 0) {
+            if ((n & 1) == 1) ans *= x;
+            x *= x;
+            n >>= 1;
         }
+        System.out.println(ans);
     }
 }
--- leetcode/src/Test.java
+++ leetcode/src/Test.java
@@ -1,33 +1,49 @@
-import java.text.ParseException;
-
 public class Test {
-    public static void main(String[] args) throws ParseException {
-        System.out.println(computeLevenshteinDistance("携程", "同程"));
-    }
-    private static int computeLevenshteinDistance(String descHotel, String poiHotel) {
-        int[][] distance = new int[descHotel.length() + 1][poiHotel.length() + 1];
-
-        for (int i = 0; i <= descHotel.length(); i++) {
-            distance[i][0] = i;
+    private int[] parent;
+    private int[] sizes;
+    public Test(int capacity) {
+        parent = new int[capacity];
+        for (int i = 0; i < capacity; i++) {
+            parent[i] = i;
         }
-        for (int j = 0; j <= poiHotel.length(); j++) {
-            distance[0][j] = j;
+        sizes = new int[capacity];
+        for (int i = 0; i < capacity; i++) {
+            sizes[i] = 1;
         }
-        for (int i = 1; i <= descHotel.length(); i++) {
-            for (int j = 1; j <= poiHotel.length(); j++) {
-                if (descHotel.charAt(i - 1) == poiHotel.charAt(j - 1)) {
-                    distance[i][j] = distance[i - 1][j - 1];
-                } else {
-                    int insert = distance[i][j - 1] + 1;
-                    int delete = distance[i - 1][j] + 1;
-                    int replace = distance[i - 1][j - 1] + 1;
-                    distance[i][j] = Math.min(Math.min(insert, delete), replace);
-                }
-            }
-        }
-        return distance[descHotel.length()][poiHotel.length()] * 100 / descHotel.length();
     }
 
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    private void union(int x, int y) {
+        int rootx = find(x);
+        int rooty = find(y);
+        if (rootx == rooty) return;
+        if (sizes[rootx] < sizes[rooty]) {
+            parent[rootx] = rooty;
+            sizes[rooty] += sizes[rootx];
+        } else {
+            parent[rooty] = rootx;
+            sizes[rootx] += sizes[rooty];
+        }
+    }
+
+    private boolean isSame(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test(12);
+        test.find(7);
+        test.union(4, 5);
+        test.union(3, 4);
+        boolean same = test.isSame(3, 5);
+        System.out.println(same);
+    }
 }
 
 
