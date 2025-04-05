22a933fa6406697685498feeaa1360dfc0bbcb51
suxiang <2585546823@qq.com>
2025-04-05 10:35:44 +0800

    重刷hot100

--- leetcode/src/栈和队列/_239_滑动窗口最大值.java
+++ leetcode/src/栈和队列/_239_滑动窗口最大值.java
@@ -2,11 +2,8 @@ package 栈和队列;
 
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class _239_滑动窗口最大值 {
-
-    //ac
     public int[] maxSlidingWindow(int[] nums, int k) {
         int len = nums.length;
         int[] res = new int[len - k + 1];
@@ -17,51 +14,14 @@ public class _239_滑动窗口最大值 {
                 deque.pollLast();
             }
             deque.offerLast(i);
-            if (deque.peekFirst() <= i - k) {//保存索引的原因
-                deque.pollFirst();//移除旧的元素
-            }
             if (i >= k - 1) {
+                if (deque.peekFirst() <= i - k) {//保存索引的原因
+                    deque.pollFirst();//移除旧的元素
+                }
                 //有k个数了
                 res[i - k + 1] = nums[deque.peekFirst()];//获取当前最大值
             }
         }
         return res;
     }
-    // 运行超时
-    public int[] maxSlidingWindow2(int[] nums, int k) {
-        Queue<Integer> queue = new LinkedList<>();
-        int[] max = new int[nums.length - k + 1];
-        int maxValue = Integer.MIN_VALUE;
-        for (int i = 0; i < k; i++) {
-            maxValue = Math.max(maxValue, nums[i]);
-            queue.offer(nums[i]);
-        }
-        max[0] = maxValue;
-
-        for (int i = k; i < nums.length; i++) {
-            int out = queue.poll();
-            queue.offer(nums[i]);
-            if (out != max[i - k]) {
-                max[i - k + 1] = Math.max(max[i - k], nums[i]);
-            } else {
-                if (nums[i] >= out) {
-                    max[i - k + 1] = nums[i];
-                } else {
-                    max[i - k + 1] = findMaxFromQueue(queue);
-                }
-            }
-        }
-
-        return max;
-    }
-
-    private int findMaxFromQueue(Queue<Integer> queue) {
-        int maxValue = Integer.MIN_VALUE;
-        for (Integer cur : queue) {
-            maxValue = Math.max(maxValue, cur);
-        }
-        return maxValue;
-    }
-
-
 }
