84df19840bf041928574beadabc81335e4aef471
suxiang <2585546823@qq.com>
2025-04-05 10:23:21 +0800

    重刷hot100

--- leetcode/src/贪心法/_53_最大子数组和.java
+++ leetcode/src/贪心法/_53_最大子数组和.java
@@ -3,17 +3,15 @@ package 贪心法;
 public class _53_最大子数组和 {
     //贪心：找局部最大和的连续子数组，得到全局的最大连续子数组的和。
     public int maxSubArray(int[] nums) {
-        int res = nums[0];
-        int preMax = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            if(preMax < 0){
-                preMax = nums[i];
+        int sum = 0;
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum > 0) {
+                sum += nums[i];
             } else {
-                preMax = preMax + nums[i];
+                sum = nums[i];
             }
-            if(res < preMax){
-                res = preMax;
-            }
+            res = Math.max(res, sum);
         }
         return res;
     }
